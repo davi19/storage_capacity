113fe9b0c7785c9152eb4a0f80188d3b02d7b762
Davi <davi19_19@hotmail.com>
2020-10-20 09:55:27 -0300

    initial ios support

--- ios/Classes/SwiftStorageCapacityPlugin.swift
+++ ios/Classes/SwiftStorageCapacityPlugin.swift
@@ -9,6 +9,31 @@ public class SwiftStorageCapacityPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+
+     if (call.method == "getPlatformVersion") {
+               result("iOS " + UIDevice.current.systemVersion)
+           }
+            else if(call.method == "getFreeSpace"){
+                if let freeSpaceInBytes = FileManagerUility.getFileSize(for: .systemFreeSize) {
+                   result(freeSpaceInBytes)
+                }
+              }
+              else if(call.method == "getOccupiedSpace"){
+ if let totalSpaceInBytes = FileManagerUility.getFileSize(for: .systemSize) {
+                 if let freeSpaceInBytes = FileManagerUility.getFileSize(for: .systemFreeSize) {
+                                    let bytesOccupied = totalSpaceInBytes - freeSpaceInBytes
+                                 }
+
+                                }
+                result(bytesOccupied)
+              } else if(call.method == "getTotalSpace"){
+                 if let totalSpaceInBytes = FileManagerUility.getFileSize(for: .systemSize) {
+                                   result(totalSpaceInBytes)
+
+                                }
+              }
+              else {
+                result.notImplemented()
+              }
   }
 }
